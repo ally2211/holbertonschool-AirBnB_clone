940b1154d32b7470d411db8515bc0876569d79e2
ally-2211 <amendieta.va@gmail.com>
2023-10-10 02:19:25 -0500

    console

--- console.py
+++ console.py
@@ -1,21 +1,32 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
+    """
+    A simple command interpreter for the AirBnB project.
+    """
+
     prompt = "(hbnb) "
 
-    def do_quit(self, line):
-        """Exit the command interpreter"""
-        return True
+    def do_quit(self, args):
+        """
+        Quit the program.
+        """
+        exit()
 
-    def do_EOF(self, line):
-        """Exit the command interpreter"""
-        print()
-        return True
+    def do_EOF(self, args):
+        """
+        Quit the program.
+        """
+        self.do_quit(args)
 
     def emptyline(self):
-        """Do nothing on empty input line"""
+        """
+        Do nothing when an empty line is entered.
+        """
         pass
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     HBNBCommand().cmdloop()
+
